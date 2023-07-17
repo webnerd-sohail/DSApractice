1e847dbcd1574bdba0ea5c1afd99c32109bd39e8
Sohail <sohailshaikh7875@gmail.com>
2023-07-17 08:39:19 +0530

    day1 file uploaded

---
+++ day1.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // int asci = 'a';
+    // cout<<asci<<endl;
+    // cout<<sizeof(asci);
+    // short int a;
+    // long int b;
+    // cout<<sizeof(a)<<endl;
+    // cout<<sizeof(b);
+
+    // int a, b, sum ;
+    // cout<<"hello sohail"<<endl;
+    // cout<<"enter 2 numbers below"<<endl;
+    // cin>>a>>b;
+    // sum = a + b;
+    // cout<<"sum is "<<sum<<endl;
+
+//     int savings;
+//     cin>>savings;
+
+//     if(savings>5000)
+//     {
+//         if(savings<7000){
+
+//         cout<<"lets do shopping"<<endl;
+//         }
+//         else{
+//             cout<<"buy phone"<<endl;
+//         }
+//     }
+//     else{
+//         cout<<"save more to reach target"<<endl;
+//     }
+
+// // find max number
+
+// int a, b, c;
+
+// cin>>a>>b>>c;
+// if(a>b){
+//     if(a>c){
+//         cout<<a<<endl;
+//     }
+//     else{
+//         cout<<c<<endl;
+//     }
+// }
+// if(b>c){
+//     cout<<b<<endl;
+// }
+// else{
+//     cout<<c<<endl;
+// }
+
+int n;
+
+cin>>n;
+
+if(n%2==0){
+    cout<<"even"<<endl;
+
+}
+else{
+
+    cout<<"odd"<<endl;
+}
+
+    return 0;
+}
